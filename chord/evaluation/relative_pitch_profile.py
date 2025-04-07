010c948f9fa2fb1267e7c8fce34194081f8eb823
Yuexuan Kong <yuexuan.kong@gmail.com>
2025-04-07 14:23:32 +0200

    eval

--- chord/evaluation/relative_pitch_profile.py
+++ chord/evaluation/relative_pitch_profile.py
@@ -13,7 +13,7 @@ import numpy.typing as npt
 import torch
 import torch.nn
 import torchaudio
-from chord.model.chromanet import ChromaNet
+from chord.model.chromanet import ChromaNet, ChromaNetSeq2Seq
 from chord.model.hcqt import HarmonicVQT, CropCQT
 import tensorflow as tf
 import tensorflow_io as tfio
@@ -45,7 +45,7 @@ def load_checkpoint(ckpt_path: str, gin_file: str, save_dict: Dict[str, Any]) ->
 
 # @gin.configurable  # type: ignore
 @tf.function(experimental_relax_shapes=True)  # type: ignore
-def load_audio_demucs(
+def load_audio(
     data: Dict[str, Any],
     sr: int,
     source: str,
@@ -55,7 +55,7 @@ def load_audio_demucs(
     """
     Load separated stems (by demucs) from path.
     """
-    def py_load_separated_audio(
+    def py_load_audio(
         song_path: tf.string,
         sr: tf.int32,
     ) -> Tuple[npt.NDArray[np.float32], int, float]:
@@ -77,7 +77,7 @@ def load_audio_demucs(
         return x.numpy()
 
     x = tf.py_function(
-        py_load_separated_audio,
+        py_load_audio,
         [data["song_path"], sr],
         (tf.float32),
     )
@@ -129,7 +129,7 @@ class RelativePitchProfile(IterableDataset):  # type: ignore
                     output_signature=taxonomy,
                     args=[self.song_path],
                     )
-                .map(lambda x: (load_audio_demucs(x, sr, source,mono, do_norm), x["song_path"]))
+                .map(lambda x: (load_audio(x, sr, source,mono, do_norm), x["song_path"]))
                 .filter(lambda x, y: tf.reduce_max(tf.abs(x["audio"])) != 0)
                 )
 
@@ -157,60 +157,42 @@ def get_embedding(
         crop_fn: CropCQT,
         batch: torch.Tensor, 
         song_path: str,
-        overlap: float=False, 
-        average: bool=True, 
+        average: bool=False, 
     ) -> Tensor:
     load_len = batch.shape[-2]
-    if overlap:
-        new_batch = tf.signal.frame(tf.transpose(batch), sr * dur, math.floor(sr * dur*(1-overlap))).numpy()
+    num_batches = math.floor(load_len / (sr * dur))
+    s = int(sr * dur)
+    if num_batches != 0:
+        proc_duration = int(sr * dur * num_batches)
+        new_batch = batch
+        new_batch = new_batch[:proc_duration, :]
         new_batch = rearrange(
-                new_batch,
-                "c b f -> b c f"
-                )
-        new_batch = torch.from_numpy(new_batch)
-    else:
-        # num_batches = math.floor(load_len / (sr * dur))
-        # s = int(sr * dur)
-        # if num_batches != 0:
-        #     proc_duration = int(sr * dur * num_batches)
-        #     new_batch = batch
-        #     new_batch = new_batch[:proc_duration, :]
-        #     new_batch = rearrange(
-        #         new_batch,
-        #         "(b s) c -> b c s",
-        #         b=num_batches,
-        #         s=s,
-        #     )
-        # else: 
+            new_batch,
+            "(b s) c -> b c s",
+            b=num_batches,
+            s=s,
+        )
+    else: 
         new_batch = batch.permute(1, 0).unsqueeze(dim=0)
     with torch.no_grad():
-        after_crop = crop_fn(hcqt(new_batch), torch.zeros(len(new_batch, )))
+        after_crop = crop_fn(hcqt(new_batch), torch.zeros(len(new_batch, ))).squeeze()
         try:
             yh = chromanet(after_crop)
+            yh = rearrange(yh, "b f t -> (b t) f").argmax(axis=1)
             return yh if not average else torch.mean(yh, dim=0).argmax()
         except:
             print(after_crop.shape)
             return torch.Tensor([-1])            
 
 
-def main(ckpt_path: str, audio_path: str, extension: str, overlap:float, average:bool) -> None:
+def main(ckpt_path: str, audio_path: str, extension: str, average:bool) -> None:
     device = set_cpu()
     ckpt = load_checkpoint(ckpt_path, "", {})
     dur, sr = [ckpt["audio"]["dur"], ckpt["audio"]["sr"]]
     num_frames = dur*sr
     model_name = "-".join(ckpt_path.split("/")[-5:-1])
-    hcqt = HarmonicVQT(
-        harmonics=eval(ckpt["gin_info"]["HarmonicVQT.harmonics"]),
-        fmin=float(ckpt["gin_info"]["HarmonicVQT.fmin"]), 
-        n_bins=int(ckpt["gin_info"]["HarmonicVQT.n_bins"]), 
-    ).to(device)
-    chromanet = ChromaNet(
-        n_bins = int(ckpt["gin_info"]["Stone.n_bins"]),
-        n_harmonics=len(eval(ckpt["gin_info"]["HarmonicVQT.harmonics"])), 
-        out_channels=eval(ckpt["gin_info"]["Stone.out_channels"]),
-        kernels=eval(ckpt["gin_info"]["Stone.kernels"]),
-        temperature=float(ckpt["gin_info"]["Stone.temperature"])
-    ).to(device)
+    hcqt = HarmonicVQT().to(device)
+    chromanet = ChromaNetSeq2Seq().to(device)
     hcqt_state_dict = {
         k.replace("hcqt.", ""): v
         for k, v in ckpt["stone"].items()
@@ -225,17 +207,17 @@ def main(ckpt_path: str, audio_path: str, extension: str, overlap:float, average
     hcqt.eval()
     chromanet.load_state_dict(ckpt["stone"])
     chromanet.eval()
-    crop_fn = CropCQT(int(ckpt["gin_info"]["Stone.n_bins"]))
+    crop_fn = CropCQT(84)
     print(
         f"\n\n Computing the relative pitch profile class for {audio_path} using the model {ckpt_path} \n"
     )
     ds = RelativePitchProfile(device, glob.glob("{}/**/*.{}".format(audio_path, extension), recursive=True), num_frames, sr)
     pbar = tqdm(iter(ds))
     results = {
-        song_path: get_embedding(sr, dur, hcqt, chromanet, crop_fn, test_batch, song_path, overlap, average).cpu().numpy()
+        song_path: get_embedding(sr, dur, hcqt, chromanet, crop_fn, test_batch, song_path, average).cpu().numpy()
         for test_batch, song_path in pbar
     }
-
+    import pdbr;pdbr.set_trace()
     path_results = os.path.join(audio_path, "relative_pitch_profile", model_name) 
     if not os.path.exists(path_results):
         os.makedirs(path_results)
@@ -256,10 +238,7 @@ def input_args() -> None:
         "-e", "--extension", type=str, default="wav", help="audio format extension"
     )
     parser.add_argument(
-        "-o", "--overlap", type=float, default=False, help="the ratio of overlap portion of the moving window"
-    )
-    parser.add_argument(
-        "-a", "--average", default=True, action="store_true", help="if calculating the average over time axis or not"
+        "-a", "--average", default=False, action="store_true", help="if calculating the average over time axis or not"
     )
     parser.add_argument(
         "-p", "--model_part", type=str, default="acc", help="accompaniment or vocal for chromanet"
@@ -271,7 +250,6 @@ def input_args() -> None:
         os.path.abspath(args.checkpoint_path),
         os.path.abspath(args.audio_path),
         args.extension,
-        args.overlap,
         args.average,
     )
 
